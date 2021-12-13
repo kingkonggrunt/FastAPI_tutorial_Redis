7f71453db44cf13d6fedd3dd60354fb0dbecd5f3
Duc Cong Duong <little-kk@hotmail.com>
2021-12-13 17:59:55 +1100

    feat: main.py for redis tutorial

---
+++ main_redis.py
@@ -0,0 +1,14 @@
+import redis
+import logging
+from pyhats.src import pyhats, redis_test
+
+def main():
+    redis_test.main()
+    pyhats.initialise_stock()
+    pyhats.test_purchase()
+    pyhats.test_purchase_all()
+    # pyhats.test_purchase_another()
+    redis_test.key_expiry()
+
+if __name__ == "__main__":
+    main()
